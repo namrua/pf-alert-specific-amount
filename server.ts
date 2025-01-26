ec9c2619954779d5e27338fd7a2ef290371d3bd5
namzau <moonloren9x@gmail.com>
2025-01-27 00:52:15 +0700

    serverless

--- server.ts
+++ server.ts
@@ -4,16 +4,4 @@ import bodyParser from "body-parser";
 import { subscribeEvent } from "./src/Handler";
 import { redisPub } from "./src/RedisService";
 
-const app: Application = express();
-const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 4665;
-app.use(cors());
-app.use(bodyParser.json());
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
-
-
-app.listen(PORT, '0.0.0.0', async () => {
-    console.log(`Server is running on port ${PORT}`);
-    subscribeEvent();
-});
+subscribeEvent();
